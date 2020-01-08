3591435c1aaf4cc7df4445469ccf545ad8624ed2
Ron Ripperger <ronripp@outlook.com>
2020-01-08 01:14:14 -0500

    Fixed time : started Server File caching for docs

--- Pages/Docs.cshtml.cs
+++ Pages/Docs.cshtml.cs
@@ -6,16 +6,22 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Dropbox.Api;
 using CStat.Data;
-using System.Web;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
 
 namespace CStat
 {
     public class DocsModel : PageModel
     {
         public CSDropBox dbox;
+        private IWebHostEnvironment hostEnv;
+        //string DropBoxFile = null;
+        //Dictionary<string, string> UrlMap = null;
+
         private string _FolderName = "";
-        public DocsModel()
+        public DocsModel(IWebHostEnvironment hstEnv)
         {
+            hostEnv = hstEnv;
             dbox = new CSDropBox(Startup.CSConfig);
         }
 
@@ -24,6 +30,22 @@ namespace CStat
             FolderName = id ?? "";
             dbox.GetFolderList(FolderName);
             ViewData["Title"] = "Docs" + FolderName;
+
+            //if (FolderName.Length > 0) { 
+            //    DropBoxFile = Path.Combine(hostEnv.WebRootPath, "DropBox_" + FolderName);
+            //    UrlMap = new Dictionary<string, string>();
+
+            //using (StreamReader sr = new StreamReader(DropBoxFile))
+            //{
+            //    while (sr.Peek() >= 0)
+            //    {
+            //        string[] fields = sr.ReadLine().Split('~');
+            //        if (fields.Length >= 2)
+            //        {
+            //            UrlMap.Add(fields[0], fields[1]);
+            //        }
+            //    }
+            //}
         }
 
         public string GetSharedFileUrl (string file)

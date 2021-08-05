d849e1f56f9e2faed1739d50ff4de9d211fd65b3
ronripp <ronripp@outlook.com>
2021-08-04 21:26:07 -0400

    Better GenTask

--- Pages/Tasks/AutoGen.cs
+++ Pages/Tasks/AutoGen.cs
@@ -22,37 +22,63 @@ namespace CStat.Pages.Tasks
 
         public bool GenTasks (IWebHostEnvironment hstEnv, int tmplId = -1)
         {
-            // Get a List of Template Tasks
+            // Get a List of Template Tasks for a specific template id(when template task is changed) ...or... ALL template tasks (tmplId = -1) when looking to generate tasks in general.
             var TemplateTasks = (tmplId != -1) ? _context.Task.Include(t => t.Person).Where(t => ((t.Type & (int)CTask.eTaskType.Template) != 0) && (t.Id == tmplId)).ToList() :
                                                  _context.Task.Include(t => t.Person).Where(t => (t.Type & (int)CTask.eTaskType.Template) != 0).OrderBy(t => t.Priority).ToList();
 
-            List<CTask> taskList = new List<CTask>();
+            // Get the range limit of time to possibly create new tasks and a list of events that fully or partially overlap this range limit of time
+            DateTime now = PropMgr.ESTNow;
+            DateTime startTime = new DateTime(now.Year, now.Month, now.Day, 0, 0, 0);
+            DateTime endTime = startTime.AddMonths(6);
+            //DateTime endTime = new DateTime(startTime.Year+1, 12, 31, 23, 59, 59);
+            DateRange lim = new DateRange(startTime, endTime);
+            List<Event> evList = GetEvents(lim);
+
+            List<CTask> AllNewTaskList = new List<CTask>(); // Create a list for collecting all potentially new tasks created for all templates in TemplateTasks
 
             // Determine what needs to be Auto generated
             foreach (var tmpl in TemplateTasks)
             {
+                // Get a list of existing tasks create from the specific template : tmpl
                 List<CTask> curCreatedTasks = _context.Task.Include(t => t.Person).Where(t => ((t.ParentTaskId == tmpl.Id) || (t.Description == tmpl.Description))).ToList();
 
-                tmpl.GetTaskType(out CTask.eTaskType dueType, out CTask.eTaskType eachType, out int dueVal);
-                DateTime now = PropMgr.ESTNow;
-                DateTime startTime = new DateTime(now.Year, now.Month, now.Day, 0, 0, 0);
-                DateTime endTime = startTime.AddMonths(6);
-                //DateTime endTime = new DateTime(startTime.Year+1, 12, 31, 23, 59, 59);
-                DateRange lim = new DateRange(startTime, endTime);
-                List<Event> evList = GetEvents(lim);
-
-                // Filter out tasks that already exist : same task id and same due date
+                // Filter out new, created tasks : n that already exist : Having the {same_task_id ...or... same_description} ...AND... {same Year, Month, Day } ...AND... {newlyCreatedTime >= currExistingTime}
+                // Note : to be safe, we keep newlyCreatedTime earlier times than currExistingTime and may add as a duplicate in some special cases.
                 List<CTask> newCreatedTasks = GetTemplateTasks(hstEnv, _context, tmpl, evList, lim).Where(n => !curCreatedTasks.Any(c => ((c.ParentTaskId == n.ParentTaskId) || (c.Description == n.Description)) &&
                                                                 c.DueDate.HasValue && n.DueDate.HasValue && (c.DueDate.Value.Year == n.DueDate.Value.Year) && (c.DueDate.Value.Month == n.DueDate.Value.Month) && 
                                                                 (c.DueDate.Value.Day == n.DueDate.Value.Day) && n.DueDate >= c.DueDate)).ToList();
 
-                if (taskList.Count > 0)
+                // Make sure the newly filtered potential tasks are assigned to a person
+                // NOTE : we cannot rely on tmpl as the only parent tasks as newCreatedTasks may contain other tasks not having t.ParentTaskId = tmpl.Id 
+                if (newCreatedTasks.Count > 0)
                 {
-                    // ensure tasks are assigned to someone
-                    TaskData taskData = TaskData.ReadTaskData(hstEnv, tmpl.Id, -1);
-                    foreach (var t in taskList)
+                    // ensure each newCreatedTask is assigned to someone and that person is avaiable
+                    // Cache Task Data by task id
+                    var TDCache = new Dictionary<int, TaskData>();
+                    var PIDList = new List<int>();
+                    foreach (var t in newCreatedTasks)
                     {
-                        if (!tmpl.PersonId.HasValue || (tmpl.PersonId <= 0))
+                        TaskData taskData;
+                        if (!TDCache.TryGetValue(t.Id, out taskData))
+                        {
+                            taskData = TaskData.ReadTaskData(hstEnv, t.Id, -1);
+                            TDCache[t.Id] = taskData;
+                        }
+
+                        // For each newCreatedTask, check for a person who is unavailable and if unavailable, nullify person id and person;
+                        if (t.PersonId.HasValue && !PIDList.Contains(t.PersonId.Value))
+                        {
+                            int pid = t.PersonId.Value;
+                            PIDList.Add(pid);
+                            Person person = (Person)_context.Person.First(p => p.Id == pid);
+                            if ((person != null) && person.Roles.HasValue && (((long)person.Roles & (long)Person.TitleRoles.Unavailable) != 0))
+                            {
+                                t.PersonId = null;
+                                t.Person = null;
+                            }
+                        }
+
+                        if (!t.PersonId.HasValue || (t.PersonId <= 0))
                         {
                             // Determine person from roles
                             var pid = CTask.ResolvePersonId(_context, taskData.Role1, taskData.Role2, taskData.Role3);
@@ -66,10 +92,10 @@ namespace CStat.Pages.Tasks
                 }
 
                 // Filter out tasks that already exist.
-                taskList.AddRange(newCreatedTasks);
+                AllNewTaskList.AddRange(newCreatedTasks);
             }
 
-            // Search for any created tasks that are assigned to a person who is now unavailable
+            // Search for any existing tasks that are assigned to a person who is now unavailable
             List<CTask> unavailTasks = _context.Task.Include(t => t.Person).Where(t => t.ParentTaskId.HasValue && (t.ParentTaskId.Value > 0) && t.PersonId.HasValue && t.Person.Roles.HasValue && (((long)t.Person.Roles & (long)Person.TitleRoles.Unavailable) != 0)).ToList();
             foreach (var ut in unavailTasks)
             {
@@ -86,7 +112,7 @@ namespace CStat.Pages.Tasks
             // TBD: Maintain AutoPersonID state properly.
 
             // Check for duplicates by same description and same day before Adding to DB.
-            var OrdTasks = taskList.OrderBy(t => t.Description).ThenBy(t => t.DueDate);
+            var OrdTasks = AllNewTaskList.OrderBy(t => t.Description).ThenBy(t => t.DueDate);
             string lastDesc = "";
             int lastYear = -1, lastMonth = -1, lastDay = -1;
             foreach (var t in OrdTasks)

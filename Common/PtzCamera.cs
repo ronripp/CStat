75c37c0d1ba0e624c0628d728ffe57e9a17a010f
ronripp <ronripp@outlook.com>
2023-04-25 21:24:51 -0400

    camera and task fix

--- Common/PtzCamera.cs
+++ Common/PtzCamera.cs
@@ -898,9 +898,9 @@ public class PtzCamera : System.IDisposable
                 req.AddHeaderProp("Accept-Encoding", "gzip, deflate, br");
 
                 if (enable)
-                    req.AddBody("[{\"cmd\": \"SetEmailV20\", \"param\": {\"Email\": {\"ssl\": 0, \"smtpPort\": 587, \"userName\": \"ronripp@outlook.com\", \"password\": \"Red35823\", \"addr1\": \"ronripp3@gmail.com\", \"addr2\": \"handiguy2@optonline.net\"}}}]");
+                    req.AddBody("[{\"cmd\": \"SetEmailV20\", \"param\": {\"Email\": {\"ssl\": 1, \"smtpPort\": 587, \"userName\": \"ronripp@outlook.com\", \"password\": \"Red35823\", \"addr1\": \"ronripp3@gmail.com\", \"addr2\": \"handiguy2@optonline.net\"}}}]");
                 else
-                    req.AddBody("[{\"cmd\": \"SetEmailV20\", \"param\": {\"Email\": {\"ssl\": 0, \"smtpPort\": 587, \"userName\": \"ronripp@outlook.com\", \"password\": \"Red35823\", \"addr1\": \"\", \"addr2\": \"\"}}}]");
+                    req.AddBody("[{\"cmd\": \"SetEmailV20\", \"param\": {\"Email\": {\"ssl\": 1, \"smtpPort\": 587, \"userName\": \"ronripp@outlook.com\", \"password\": \"Red35823\", \"addr1\": \"\", \"addr2\": \"\"}}}]");
 
                 var sResult = req.SendForString();
                 if (String.IsNullOrEmpty(sResult))
